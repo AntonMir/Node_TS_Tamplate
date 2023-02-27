d6cc50ae75adf2b93ef43623c20ef2fb2c74f268
AntonMir <toshik124@rambler.ru>
2023-02-27 17:54:37 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+import log4js from 'log4js'
+
+// Логирование
+const logger = log4js.getLogger()
+logger.level = process.env.LOG_LEVEL
+logger.info('log4js log info')
+logger.debug('log4js log debug')
+logger.error('log4js log error')
+
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+ 
+app.listen(port, () => {
+    console.log(`Server start on port ${port}`)
+})
